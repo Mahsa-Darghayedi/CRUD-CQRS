d9e20b1c1f6b42013da5b6315f930b8308c39540
Mahsa Darghayedi <m.darghayedi@gmail.com>
2022-07-07 16:21:46 +0300

    Project architecture modification
    Changing project architecture to clean architecture

---
+++ CRUD-CQRS/CRUD-CQRS.Test.Application/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CRUD-CQRS.Test.Application")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CRUD-CQRS.Test.Application")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c70b510-3a8e-45fd-8eba-ce19bbd0bb01")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CRUD-CQRS/CRUD-CQRS.Test.Application/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CRUD_CQRS.Test.Application
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
